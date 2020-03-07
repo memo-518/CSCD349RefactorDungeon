7afb08df5f3713220a4dfb14a0c516dcec5ee46f
memo-518 <57207499+memo-518@users.noreply.github.com>
2020-03-06 21:45:37 -0800

    Update and rename Thief.java to ThiefHero.java

---
+++ ThiefHero.java
@@ -0,0 +1,36 @@
+
+
+public class SorceressHero implements HeroBehavior {
+
+    public SorceressHero(){}
+
+    public void chooseHero(DungeonCharacter opponent, DungeonCharacter hero)
+    {
+	    int hPoints;
+
+		hPoints = (int)(Math.random() * (hero.damageMax - hero.damageMin + 1)) + hero.damageMin;
+		hero.addHitPoints(hPoints);
+		System.out.println(hero.name + " added [" + hPoints + "] points.\n"
+							+ "Total hit points remaining are: "
+							+ hero.hitPoints);
+		 System.out.println();
+
+    }//end increaseHitPoints method
+
+//-----------------------------------------------------------------
+	public void attack(DungeonCharacter opponent, DungeonCharacter hero)
+	{
+		System.out.println(hero.name + " casts a spell of fireball at " +
+							opponent.getName() + ":");
+	}//end override of attack method
+
+    public int prompt() {
+        System.out.println("1. Attack Opponent");
+        System.out.println("2. Increase Hit Points");
+        System.out.print("Choose an option: ");
+        int choice = Keyboard.readInt();
+        return choice;
+    }
+
+//----------------------------------------------------------------
+}
